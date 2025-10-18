0570fa6ab89bf0f28b2a6bd0753febf4868f7b32
bruhtalitiets <fannach.s@hotmail.com>
2025-10-18 11:54:52 +0200

    Attempt to  initiate input value and test with add operator

---
+++ index.js
@@ -0,0 +1,41 @@
+function add (a, b){
+  return a + b; 
+}
+
+console.log(add(3,4));
+
+const display = document.querySelector(".container");
+
+const buttons = document.querySelectorAll("button");
+
+const buttonHtml = buttons.innerHTML;
+
+const displayData= []
+
+const num1 = [];
+const opp = [];
+const num2 = [];
+const currentValue = [];
+
+function add (a, b){
+  return a + b
+}
+
+
+  buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    if 
+
+ 
+  });
+});
+
+
+
+
+
+
+
+
+
