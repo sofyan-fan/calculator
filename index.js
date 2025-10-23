3eea2343c1076513e8a82d06abc8f259cca902b3
bruhtalitiets <fannach.s@hotmail.com>
2025-10-23 18:15:41 +0200

    Display, store numbers and add operator test works

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
-function add (a, b){
-  return a + b; 
-}
-
-console.log(add(3,4));
 
 const display = document.querySelector(".container");
+const buttons = document.querySelectorAll(".number");
+const clear = document.querySelector(".clear")
+const operator = document.querySelector(".operator");
+const equal = document.querySelector(".equal");
 
-const buttons = document.querySelectorAll("button");
-
-const buttonHtml = buttons.innerHTML;
-
-const displayData= []
-
-const num1 = [];
-const opp = [];
-const num2 = [];
-const currentValue = [];
+let num1; 
+let num2;
+let operatorClicked = false;
 
 function add (a, b){
-  return a + b
+  return a + b;
 }
 
 
-  buttons.forEach(button => {
-  button.addEventListener("click", () => {
 
-    if 
+console.log(operatorClicked)
+
+
+
+// Click number to show on display
+buttons.forEach(function(button){
+  button.addEventListener('click', () =>{
+    display.textContent += button.textContent;
+  })
+});
+
+operator.addEventListener("click", () =>{
+  num1 = display.textContent;
+  console.log(num1);
+  display.textContent = "";
+});
+
+equal.addEventListener("click", () =>{
+
+  num2 = display.textContent;
+  
+
+  display.textContent = add(parseInt(num1), parseInt(num2));
+})
+
+
+
+//Clear display
+clear.addEventListener("click", () =>{
+  display.textContent = " ";
+}
+);
+
+//Store 2 numbers
+
+
+
+
 
  
-  });
-});
+
+
+
+
+
+
+
+function add (a, b){
+  return a + b;
+}
+
+
+
+
 
 
 
